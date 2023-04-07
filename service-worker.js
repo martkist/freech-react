35cc04c8e48b3e47d2b70bf3f7fb360ba27286bb
blockmartkist <blockmartkist@protonmail.com>
2023-04-07 22:36:16 +0200

    Disable service worker cache for now

--- service-worker.js
+++ service-worker.js
@@ -36,10 +36,10 @@ self.addEventListener('install', (event) => {
 });
 
 // Fetch event
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
