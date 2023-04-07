0262acf8813009c16e5f2299a9249d5cd939b078
blockmartkist <blockmartkist@protonmail.com>
2023-04-07 21:51:58 +0200

    Update freech-lib.js

--- build/freech-lib.js
+++ build/freech-lib.js
@@ -69169,7 +69169,7 @@ var bs58check = require('bs58check');*/
 
 var freech_network = Bitcoin.networks.bitcoin;
 
-freech_network.messagePrefix= '\x18freech Signed Message:\n';
+freech_network.messagePrefix= '\x17freech Signed Message:\n';
 
 /**
  * Describes the public key of a user.
@@ -69951,7 +69951,7 @@ FreechTorrent.prototype.fillPostsCache = function (id,cbfunc) {
 
 var Bitcoin = require('bitcoinjs-lib');
 var freech_network = Bitcoin.networks.bitcoin;
-freech_network.messagePrefix= '\x18freech Signed Message:\n';
+freech_network.messagePrefix= '\x17freech Signed Message:\n';
 
 var FreechResource = require("./FreechResource.js");
 var Freech = new FreechResource("freech",{});
