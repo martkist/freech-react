165ab97cef9b3f68a129dbbfbcac5d2903d9bb72
blockmartkist <blockmartkist@protonmail.com>
2023-04-11 19:56:45 +0200

    Re-enable PWA

--- service-worker.js
+++ service-worker.js
@@ -27,19 +27,19 @@ const assetsToCache = [
 ];
 
 // Install event
-// self.addEventListener('install', (event) => {
-//   event.waitUntil(
-//     caches.open(cacheName).then((cache) => {
-//       return cache.addAll(assetsToCache);
-//     })
-//   );
-// });
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
 
 // Fetch event
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((response) => {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
